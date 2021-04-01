b7291b08d0d56aefaeb2e24a350b3a879e0696b3
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 01:19:20 +0200

    news handler is ez

--- src/GameServer/Handle/NewsHandler.php
+++ src/GameServer/Handle/NewsHandler.php
@@ -5,6 +5,7 @@ namespace oldsu_stream_server\GameServer\Handlers;
 class NewsHandler
 {
     public static function Handle($connection, $request){
-
+        //Return Server Date
+        $connection->send(date("Y-m-d"));
     }
 }
