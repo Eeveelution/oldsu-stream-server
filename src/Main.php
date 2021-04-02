363d31e1ea3e2ebc966ea73b31795fa036de7000
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 15:06:02 +0200

    database stuffs beginning

--- src/Main.php
+++ src/Main.php
@@ -1,7 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/sergeytsalkov/meekrodb/db.class.php';
 require_once __DIR__ . '/MainServer.php';
+
+DB::$user = env("MYSQL_USERNAME");
+DB::$password = env("MYSQL_PASSWORD");
+DB::$dbName = env("MYSQL_DATABASE");
+DB::$host = env("MYSQL_LOCATION");
+
 //Create and Start MainServer
 $server = new oldsu_stream_server\MainServer("http://127.0.0.1:80");
 $server->start();
