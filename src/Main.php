dcd2488fd4a5ff3438ed94b06757b7dc486a3372
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 15:45:01 +0200

    warnings are exceptions now

--- src/Main.php
+++ src/Main.php
@@ -4,6 +4,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/sergeytsalkov/meekrodb/db.class.php';
 require_once __DIR__ . '/MainServer.php';
 
+set_error_handler(static function($errno, $errstr, $errfile, $errline) {
+	// error was suppressed with the @-operator
+	if (0 === error_reporting()) {
+		return false;
+	}
+
+	throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
 DB::$user = getenv("MYSQL_USERNAME");
 DB::$password = getenv("MYSQL_PASSWORD");
 DB::$dbName = getenv("MYSQL_DATABASE");
