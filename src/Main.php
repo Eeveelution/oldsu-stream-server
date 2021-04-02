4ae549d0d9f1365076fb8b33b21e272625434b23
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 15:25:43 +0200

    its not env u stoopid

--- src/Main.php
+++ src/Main.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/sergeytsalkov/meekrodb/db.class.php';
 require_once __DIR__ . '/MainServer.php';
 
-DB::$user = env("MYSQL_USERNAME");
-DB::$password = env("MYSQL_PASSWORD");
-DB::$dbName = env("MYSQL_DATABASE");
-DB::$host = env("MYSQL_LOCATION");
+DB::$user = getenv("MYSQL_USERNAME");
+DB::$password = getenv("MYSQL_PASSWORD");
+DB::$dbName = getenv("MYSQL_DATABASE");
+DB::$host = getenv("MYSQL_LOCATION");
 
 //Create and Start MainServer
 $server = new oldsu_stream_server\MainServer("http://127.0.0.1:80");
