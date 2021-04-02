c0c55b10112177aa6abf40d1970a71ea8337ec62
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 18:08:40 +0200

    environment variables actually work now lmao

--- src/GameServer/Handlers/LeaderboardHandler.php
+++ src/GameServer/Handlers/LeaderboardHandler.php
@@ -4,6 +4,7 @@ namespace oldsu_stream_server\GameServer\Handlers;
 
 use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
+use DB;
 
 class LeaderboardHandler
 {
@@ -15,6 +16,17 @@ class LeaderboardHandler
 	 */
     public static function Handle($connection, $request) : void
 	{
-
+		$return = "";
+		//Query all Beatmap Packs
+		$packIds = DB::query("SELECT LocalID FROM stream_packs");
+		//Go through all packIds and Write
+		foreach($packIds as $id){
+			//Query and Write Pack
+			$pack = \MapPack::GetPackById($id["LocalID"]);
+			//Write
+			$return .= $pack->Write();
+		}
+		//Send
+		$connection->send($return);
     }
 }
--- src/Main.php
+++ src/Main.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/sergeytsalkov/meekrodb/db.class.php';
 require_once __DIR__ . '/MainServer.php';
 
+\Dotenv\Dotenv::createImmutable(__DIR__."/..")->load();
+
 set_error_handler(static function($errno, $errstr, $errfile, $errline) {
 	// error was suppressed with the @-operator
 	if (0 === error_reporting()) {
@@ -13,11 +15,11 @@ set_error_handler(static function($errno, $errstr, $errfile, $errline) {
 	throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 });
 
-DB::$user = getenv("MYSQL_USERNAME");
-DB::$password = getenv("MYSQL_PASSWORD");
-DB::$dbName = getenv("MYSQL_DATABASE");
-DB::$host = getenv("MYSQL_LOCATION");
+DB::$user = $_ENV["MYSQL_USERNAME"];
+DB::$password = $_ENV["MYSQL_PASSWORD"];
+DB::$dbName = $_ENV["MYSQL_DATABASE"];
+DB::$host = $_ENV["MYSQL_LOCATION"];
 
 //Create and Start MainServer
-$server = new oldsu_stream_server\MainServer(getenv("SERVER_LOCATION"));
+$server = new oldsu_stream_server\MainServer($_ENV["SERVER_LOCATION"]);
 $server->start();
