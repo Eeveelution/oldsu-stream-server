95a5dbc473535656f87af8691895b323a1cff5f2
Eeveelution <developerbrzoza@gmail.com>
2021-04-02 17:29:35 +0200

    test data

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -2,3 +2,5 @@ INSERT INTO stream_beatmaps (Filename, Revision, RankedStatus, Difficulty, Local
 INSERT INTO stream_beatmaps (Filename, Revision, RankedStatus, Difficulty, LocalBeatmapsetID) VALUES ("Masayuki Suzuki - DADDY! DADDY! DO! feat. Airi Suzuki - TV Size (Eevee).osz2", "1.0", "Ranked", 2, 1);
 INSERT INTO stream_beatmaps (Filename, Revision, RankedStatus, Difficulty, LocalBeatmapsetID) VALUES ("Masayuki Suzuki - DADDY! DADDY! DO! feat. Airi Suzuki - TV Size (Eevee).osz2", "1.0", "Ranked", 3, 1);
 INSERT INTO stream_beatmaps (Filename, Revision, RankedStatus, Difficulty, LocalBeatmapsetID) VALUES ("Masayuki Suzuki - DADDY! DADDY! DO! feat. Airi Suzuki - TV Size (Eevee).osz2", "1.0", "Ranked", 4, 1);
+
+INSERT INTO stream_packs (PackID, PackName, Beatmaps) VALUES ("EeveePack", "Eevee's Anime Pack", "1;2;3;4");
